756362928b7ae806837c2945f6ad7c923a1b3556
Doğan Derya <doganderya59@gmail.com>
2024-09-22 17:37:25 +0300

    Version change

--- bin/hdrunpack.js
+++ bin/hdrunpack.js
@@ -1,20 +1,17 @@
 #!/usr/bin/env node
 /* eslint-disable no-process-exit, no-empty */
 
-const program = require('commander');
-const hdrunpack = require('../lib/hdrunpack');
-const path = require('path');
+const program = require("commander");
+const hdrunpack = require("../lib/hdrunpack");
+const path = require("path");
 
-program
-  .usage('<file>')
-  .version('1.0.1')
-  .parse(process.argv);
+program.usage("<file>").version("1.0.2").parse(process.argv);
 
 if (!program.args.length) return program.help();
 
 let arg = program.args[0];
 let location = path.resolve(arg);
 
-hdrunpack(location).catch(x => {
-  console.error('an Error occurred!, ' + x.message);
-})
+hdrunpack(location).catch((x) => {
+  console.error("an Error occurred!, " + x.message);
+});
